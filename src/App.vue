26d5b47b7e6b06e2f089448668500aae34102ea3
tai-le-newhorizon <119556415+tai-le-newhorizon@users.noreply.github.com>
2025-02-16 22:56:04 +0700

    chore: bump version

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,11 @@ const downloadFile = (filename: string, content: string) => {
     document.body.removeChild(link);
 };
 
+const downloadDbml = () => {
+    const content = store.dbmlRaw;
+    const filename = `dbml-${new Date().toISOString().split('T')[0]}.sql`;
+    downloadFile(filename, content);
+}
 const downloadSql = () => {
     const content = store.DbmlSql;
     const filename = `sql-${new Date().toISOString().split('T')[0]}.sql`;
@@ -28,6 +33,7 @@ const downloadSqlite = () => {
 <template>
     <header class="comp-layer">
         <span class="logo">DBML</span>
+        <div class="menu" @click="downloadDbml">Dbml<i class="icon-arrow-down-circle"></i></div>
         <div class="menu" @click="downloadSqlite">Sqlite<i class="icon-arrow-down-circle"></i></div>
         <div class="menu" @click="downloadSql">Sql<i class="icon-arrow-down-circle"></i></div>
         <!-- <router-link :to="{ name: 'view-page' }">View</router-link> -->
